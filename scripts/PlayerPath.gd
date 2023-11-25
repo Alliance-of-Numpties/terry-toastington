8adf9c3b580319e1398a513e5d5154afacfd85fc
Olli Johnson <ollijohnson93@gmail.com>
2023-11-25 14:23:51 +0000

    Make the Ghoast follow the players facing direction

--- scripts/PlayerPath.gd
+++ scripts/PlayerPath.gd
@@ -8,10 +8,12 @@ class_name PlayerPath
 class TimedPoint:
 	var time: float
 	var position: Vector2
+	var x_scale: float # Tracked for the facing direction
 
-	func _init(_time: float, _position: Vector2):
+	func _init(_time: float, _position: Vector2, _x_scale: float):
 		time = _time
 		position = _position
+		x_scale = _x_scale
 
 class Recording:
 	var start_time: float
@@ -21,7 +23,7 @@ class Recording:
 		start_time = _start_time
 		points = _points
 
-	func get_position_for_time(time: float) -> Vector2:
+	func get_point_for_time(time: float) -> TimedPoint:
 		# Wildly inefficient for now, but nevermind...
 		var prev_point: TimedPoint = null
 		var next_point: TimedPoint = null
@@ -32,14 +34,14 @@ class Recording:
 				next_point = point
 				break
 		if prev_point == null:
-			return next_point.position
+			return next_point
 		if next_point == null:
-			return prev_point.position
+			return prev_point
 		var time_delta = next_point.time - prev_point.time
 		var time_progress = (time - prev_point.time) / time_delta
-		return prev_point.position.lerp(next_point.position, time_progress)
+		return TimedPoint.new(time, prev_point.position.lerp(next_point.position, time_progress), prev_point.x_scale)
 
-@onready var recording: Recording = Recording.new(Time.get_ticks_msec(), [TimedPoint.new(0.0, player.position)])
+@onready var recording: Recording = Recording.new(Time.get_ticks_msec(), [TimedPoint.new(0.0, player.position, player.model.scale.x)])
 var timer: Timer
 @export var spawn_time: float = 3.0
 
@@ -51,7 +53,7 @@ func _ready():
 
 func _physics_process(_delta):
 	if recording != null:
-		var new_point = TimedPoint.new(Time.get_ticks_msec() - recording.start_time, player.position)
+		var new_point = TimedPoint.new(Time.get_ticks_msec() - recording.start_time, player.position, player.model.scale.x)
 		recording.points.append(new_point)
 
 func spawn_child():
--- scripts/PlayerPathFollow.gd
+++ scripts/PlayerPathFollow.gd
@@ -7,4 +7,6 @@ var start_time: int = 0
 
 func _physics_process(_delta):
 	if recording != null:
-		position = recording.get_position_for_time(Time.get_ticks_msec() - start_time)
+		var timed_point = recording.get_point_for_time(Time.get_ticks_msec() - start_time)
+		position = timed_point.position
+		scale.x = sign(timed_point.x_scale) * abs(scale.x)
