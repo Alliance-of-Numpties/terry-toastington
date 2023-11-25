8a9adf6118d08e669332ab84acdb441fdc01bb20
Olli Johnson <ollijohnson93@gmail.com>
2023-11-25 09:42:17 +0000

    Add the ability to slide on walls

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,6 +7,7 @@ signal death
 @export var speed = 500.0
 @export var jump_velocity = -900.0
 @export var jam_points = 0
+@export var wall_slide_speed_limit = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,11 +16,20 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if not is_on_wall():
+			velocity.y += gravity * delta
+		else:
+			velocity.y += gravity * delta
+			# Continue jumping with regular gravity if still moving up,
+			# otherwise use the wall slide gravity modifier.
+			velocity.y = min(velocity.y, wall_slide_speed_limit)
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif is_on_wall():
+			velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
