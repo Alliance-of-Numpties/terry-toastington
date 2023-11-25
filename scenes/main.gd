ae625fb091f39648f2f8f362404a6fae1639c162
Olli Johnson <ollijohnson93@gmail.com>
2023-11-25 17:19:22 +0000

    Add some music and downloaded sound effects

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
 var current_scene
+var current_level_node
 
 
 func _ready():
 	var level1_scene = load("res://scenes/level1.tscn")
 	current_scene = level1_scene
 	print(current_scene)
-	add_child(current_scene.instantiate())
+	current_level_node = current_scene.instantiate()
+	add_child(current_level_node)
 
 
 func reload_current_scene():
-	var current = get_child(0)
-	remove_child(current)
-	current.queue_free()
-	add_child(current_scene.instantiate())
+	remove_child(current_level_node)
+	current_level_node.queue_free()
+	current_level_node = current_scene.instantiate()
+	add_child(current_level_node)
