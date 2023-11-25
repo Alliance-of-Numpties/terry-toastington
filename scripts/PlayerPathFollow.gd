09e5fa4ed5762a7ad5ac89b4f67e3767976a7ca9
Olli Johnson <ollijohnson93@gmail.com>
2023-11-25 22:54:01 +0000

    Add some effects to the jam spawning

--- scripts/PlayerPathFollow.gd
+++ scripts/PlayerPathFollow.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 class_name PlayerPathFollow
 
+@export var playback_speed: float = 0.6
+
 var recording: PlayerPath.Recording = null
 var run_time: int = 0
 
 func _physics_process(delta):
 	if recording != null:
-		run_time += delta * 1000 # msecs
+		run_time += playback_speed * delta * 1000 # msecs
 		var timed_point = recording.get_point_for_time(run_time)
 		position = timed_point.position
 		scale.x = sign(timed_point.x_scale) * abs(scale.x)
--- scripts/spawn_jam.gd
+++ scripts/spawn_jam.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
+@export var jam_scene: PackedScene
+@export var effect_scene: PackedScene
 
 func get_player():
 	return get_tree().get_first_node_in_group("Player")
-
 
 func choose_random(list):
 	return list[randi() % list.size()]
 
 
 func spawn_jam():
-	var jam_scene = load("res://scenes/jam.tscn")
 	var spawn_points = get_tree().get_nodes_in_group("Jam Spawn Points")
 	var target_spawn = choose_random(spawn_points)
 
@@ -20,6 +20,11 @@ func spawn_jam():
 	var jam_instance = jam_scene.instantiate()
 	add_sibling(jam_instance)
 	jam_instance.global_position = target_spawn.global_position
+	var effect_instance = effect_scene.instantiate()
+	add_sibling(effect_instance)
+	effect_instance.global_position = target_spawn.global_position
+	effect_instance.emitting = true
+	print(jam_instance)
 
 
 func _on_player_spawner_spawned_player():
