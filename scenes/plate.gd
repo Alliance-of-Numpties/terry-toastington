e4b9e1ac889571527d2d4a775d154a2c445e6df0
Olli Johnson <ollijohnson93@gmail.com>
2023-11-26 10:46:10 +0000

    Add a sparkle effect to the plate once it is active

--- scenes/plate.gd
+++ scenes/plate.gd
@@ -3,3 +3,12 @@ extends StaticBody2D
 
 func _on_area_2d_body_entered(penetrator):
 	penetrator.collided_with_plate(self)
+
+var player = null
+
+func _ready():
+	var level = get_parent().get_parent()
+	level.all_jam_collected.connect(_on_all_jam_collected)
+
+func _on_all_jam_collected():
+	$AnimationPlayer.play("active")
